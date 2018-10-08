7973263fd87d0029795727cecca5d2e233235ea2
lucarebuffi <lrebuffi@anl.gov>
2018-10-08 11:12:04 -0500

    SRW CRL

--- wiselib2/version.py
+++ wiselib2/version.py
@@ -3,7 +3,7 @@
 short_version = '1.0.2'
 version = '1.0.2'
 full_version = '1.0.2'
-git_revision = '49dd0605c19a4b0cbf1332d22532fe7f64b0df15'
+git_revision = 'd2b81f3c0e4cdb7f10431ef9a4555781b51dfb51'
 release = True
 
 if not release:
